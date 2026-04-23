1b78f3acd51a800fec86c8e72db3549450a02750
poltorprogrammer <poltorprogrammer@users.noreply.github.com>
2026-04-23 17:24:02 +0200

    Bypass de bots: Cambiando estrategia a cliente iOS (iPhone)

--- server/app.py
+++ server/app.py
@@ -26,13 +26,14 @@ def get_info():
         'quiet': True,
         'no_warnings': True,
         'format': 'bestaudio/best',
-        # Intentamos identificarnos como cliente de Android para evitar el bloqueo de bots
+        # Intentamos identificarnos como cliente de iOS para evitar el bloqueo de bots
         'extractor_args': {
             'youtube': {
-                'player_client': ['android', 'web'],
+                'player_client': ['ios', 'web'],
                 'skip': ['webpage', 'configs'],
             }
-        }
+        },
+        'user_agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1'
     }
 
     try:
@@ -67,10 +68,11 @@ def download():
         'quiet': True,
         'extractor_args': {
             'youtube': {
-                'player_client': ['android', 'web'],
+                'player_client': ['ios', 'web'],
                 'skip': ['webpage', 'configs'],
             }
-        }
+        },
+        'user_agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1'
     }
 
     try:
